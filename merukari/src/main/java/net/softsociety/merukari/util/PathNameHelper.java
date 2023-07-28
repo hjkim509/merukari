f1965c2b37c6043377b7fe88771699e84830ea67
ihunjae94 <ihunjae94@gmail.com>
2023-07-28 17:15:56 +0900

    [v0.0.1] Edit PathNameHelper.java

--- merukari/src/main/java/net/softsociety/merukari/util/PathNameHelper.java
+++ merukari/src/main/java/net/softsociety/merukari/util/PathNameHelper.java
@@ -5,5 +5,4 @@ public class PathNameHelper
 	public static String BOARD = "board";
 	public static String BOARDVIEW = "boardView";
 	public static String BOARDSEARCH = "boardSearch";
-	public static String AAAA = "AAAA";s
 }
